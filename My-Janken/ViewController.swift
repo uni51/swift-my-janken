f550e131e8544c50db19715089a846c47297b61e
uni51 <uni51@users.noreply.github.com>
2019-05-17 16:35:44 +0900

    finish 3-7

--- My-Janken/ViewController.swift
+++ My-Janken/ViewController.swift
@@ -15,11 +15,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    // じゃんけん（数字）
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+        // 新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = Int(arc4random_uniform(3))
+        
+        repeat {
+            newAnswerNumber = Int(arc4random_uniform(3))
+        } while answerNumber == newAnswerNumber
+        
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0 {
+            // グー
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named:"gu")
+        } else if answerNumber == 1 {
+            // チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named:"choki")
+        } else if answerNumber == 2 {
+            // パー
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named:"pa")
+        }
+
     }
-
-
+    
 }
 
